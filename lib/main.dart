400843d13deea966b884f4962aec970d65c73afb
Rafa <rafaelasimoes2203@mail.com>
2023-08-25 14:15:33 -0300

    marcação das tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,25 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(TaskListApp());
+void main() => runApp(TodoApp());
 
-class TaskListApp extends StatelessWidget {
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lista de Tarefas',
-      theme: ThemeData(primarySwatch: Colors.teal),
-      home: TaskListScreen(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: TodoList(),
     );
   }
 }
 
-class TaskListScreen extends StatefulWidget {
+class TodoList extends StatefulWidget {
   @override
-  _TaskListScreenState createState() => _TaskListScreenState();
+  _TodoListState createState() => _TodoListState();
 }
 
-class _TaskListScreenState extends State<TaskListScreen> {
+class _TodoListState extends State<TodoList> {
   List<Map<String, dynamic>> tasks = [];
 
   TextEditingController _newTaskController = TextEditingController();
@@ -55,18 +55,32 @@ class _TaskListScreenState extends State<TaskListScreen> {
     return '${directory.path}/tasks.json';
   }
 
-  void toggleTaskCompletion(int index) {
+  void _toggleTask(int index) {
     setState(() {
       tasks[index]['completed'] = !tasks[index]['completed'];
+
+      if (tasks[index]['completed']) {
+        final completedTask = tasks.removeAt(index);
+        tasks.add(completedTask);
+      }
     });
     _saveTasks(); // Save tasks after toggling
   }
 
-  void addNewTask(String newTask) {
+  void _addTask(String taskTitle) {
+    if (taskTitle.isNotEmpty) {
+      setState(() {
+        tasks.add({'task': taskTitle, 'completed': false});
+      });
+      _saveTasks(); // Save tasks after adding
+    }
+  }
+
+  void _deleteTask(int index) {
     setState(() {
-      tasks.add({'task': newTask, 'completed': false});
+      tasks.removeAt(index);
     });
-    _saveTasks(); // Save tasks after adding
+    _saveTasks(); // Save tasks after deleting
   }
 
   @override
@@ -75,67 +89,58 @@ class _TaskListScreenState extends State<TaskListScreen> {
       appBar: AppBar(
         title: Text('Lista de Tarefas'),
       ),
-      body: ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          String task = tasks[index]['task'];
-          bool isCompleted = tasks[index]['completed'];
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                String task = tasks[index]['task'];
+                bool isCompleted = tasks[index]['completed'];
 
-          return Dismissible(
-            key: UniqueKey(),
-            direction: DismissDirection.horizontal,
-            onDismissed: (direction) {
-              setState(() {
-                tasks.removeAt(index);
-              });
-              _saveTasks(); // Save tasks after removing
-            },
-            child: ListTile(
-              title: Text(
-                task,
-                style: TextStyle(
-                  color: isCompleted ? Colors.green : Colors.teal,
-                  decoration: isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
-                ),
-              ),
-              leading: Checkbox(
-                value: isCompleted,
-                onChanged: (value) {
-                  toggleTaskCompletion(index);
-                },
-              ),
-              trailing: isCompleted ? Icon(Icons.check, color: Colors.green) : null,
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('Adicionar Tarefa'),
-                content: TextField(
-                  controller: _newTaskController,
-                  decoration: InputDecoration(labelText: 'Digite a nova tarefa'),
-                ),
-                actions: [
-                  ElevatedButton(
-                    onPressed: () {
-                      addNewTask(_newTaskController.text);
-                      _newTaskController.clear();
-                      Navigator.of(context).pop();
-                    },
-                    child: Text('Adicionar'),
+                return Dismissible(
+                  key: UniqueKey(),
+                  onDismissed: (_) => _deleteTask(index),
+                  child: GestureDetector(
+                    onTap: () => _toggleTask(index),
+                    child: ListTile(
+                      title: Text(
+                        task,
+                        style: TextStyle(
+                          decoration: isCompleted
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                    ),
                   ),
-                ],
-              );
-            },
-          );
-        },
-        label: Text('Adicionar Tarefa'),
-        icon: Icon(Icons.add),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _newTaskController,
+                    decoration: InputDecoration(
+                      hintText: 'Adicionar tarefa...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    _addTask(_newTaskController.text);
+                    _newTaskController.clear();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
