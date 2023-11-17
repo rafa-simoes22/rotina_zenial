e6dc7a609dd30fa73c5c2654aad89d9575fc522b
Marcella <129318421+marcella28@users.noreply.github.com>
2023-11-17 15:09:40 -0300

    ui/ux 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'cadastro.dart';
 import 'login.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,10 +21,18 @@ class InicioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF446A56),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // Adiciona a logo aqui
+            Image.asset(
+              'assets/logo.png', // Substitua pelo caminho real da sua logo
+              width: 250,
+              height: 250,
+            ),
+            SizedBox(height: 20), // Espaçamento entre a logo e os botões
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/cadastro');
