faf1c467dbb62a3a84b3a273be2979afcd8bdc54
Rafaela Simões <rafaelasimoes2203@gmail.com>
2023-12-05 17:17:11 -0300

    imagem de background tela main

--- lib/main.dart
+++ lib/main.dart
@@ -21,52 +21,52 @@ class InicioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFD8FFBE),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(width: 3, color: Colors.black),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/inicio.png'), 
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/cadastro');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF97E366),
+                  minimumSize: Size(200, 50),
+                  side: BorderSide(width: 1, color: Colors.black),
+                ),
+                child: Text(
+                  'Cadastro',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              child: Image.asset(
-                'assets/logo.png',
-                width: 250,
-                height: 250,
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF97E366),
+                  minimumSize: Size(200, 50),
+                  side: BorderSide(width: 1, color: Colors.black),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-            ),
-            SizedBox(height: 16), // Use relative spacing
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/cadastro');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF97E366),
-                minimumSize: Size(200, 50),
-                side: BorderSide(width: 1, color: Colors.black),
-              ),
-              child: Text(
-                'Cadastro',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            SizedBox(height: 16), // Use relative spacing
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF97E366),
-                minimumSize: Size(200, 50),
-                side: BorderSide(width: 1, color: Colors.black),
-              ),
-              child: Text(
-                'Login',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
